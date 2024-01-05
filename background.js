2aeb655b8b8d55b7883130605bd98838b70b4fd2
Mark Kurti <kurtimark41@gmail.com>
2024-01-05 23:43:36 +0200

    Demo 5
    test

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
 // background.js
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'www.youtube.com' }
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'www.twitch.tv' }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
 let isTimeValid;
 
 setInterval(() => {
