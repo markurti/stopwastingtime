66fed5388885beb1a0bf0426ff3c0f0b2d5db575
Mark Kurti <kurtimark41@gmail.com>
2024-01-05 23:38:01 +0200

    Demo 3
    Test

--- background.js
+++ background.js
@@ -1,27 +1,12 @@
 // background.js
-let isTimeValid;
-
-chrome.runtime.onConnect.addListener((port) => {
-  port.onMessage.addListener((msg) => {
-    if (msg.action === "checkTimeAndOpenLink") {
-      checkTimeAndOpenLink(msg.link, port.sender.tab.id);
-    }
-  });
-});
-
-function checkTimeAndOpenLink(link, tabId) {
-  if (isTimeValid) {
-    chrome.runtime.sendMessage({ action: "openLinkInBackground", link: link });
-  } else {
-    chrome.tabs.sendMessage(tabId, { action: "showAlert", message: "Sorry, it's not the designated time to open YouTube and Twitch links." });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "openLinkInBackground") {
+    openLinkInBackground(request.link);
   }
-}
+});
 
 function openLinkInBackground(link) {
   chrome.tabs.create({ url: link }, function (tab) {
     // Tab created
   });
 }
-
-// Set isTimeValid based on your logic (e.g., check the time)
-isTimeValid = true;  // Replace with your logic to determine if the time is valid
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,16 @@
 // contentScript.js
-const port = chrome.runtime.connect({ name: "content-script" });
-
-port.postMessage({
-  action: "checkTimeAndOpenLink",
-  link: window.location.href,
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "checkTimeAndOpenLink") {
+    checkTimeAndOpenLink(request.link);
+  }
 });
+
+function checkTimeAndOpenLink(link) {
+  chrome.runtime.getBackgroundPage(function (backgroundPage) {
+    if (backgroundPage.is_valid_time()) {
+      backgroundPage.open_link(link);
+    } else {
+      alert("Sorry, it's not the designated time to open YouTube and Twitch links.");
+    }
+  });
+}
